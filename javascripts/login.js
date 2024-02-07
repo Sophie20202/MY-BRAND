8a1812c020abb13292e7057dd3829e3cda714732
Sophie20202 <sofidele12@gmail.com>
2024-02-07 10:23:56 -1200

    dfchdfgwfhgdgyfwg

--- javascripts/login.js
+++ javascripts/login.js
@@ -4,8 +4,8 @@ function validateForm() {
     var password = document.getElementById('password').value;
     console.log(password,username)
     if (username === 'sofidele12@gmail.com' && password === 'sophie1992@') {
-      // alert('Login successfull!');
-      window.location.href="adminpanel.html"
+      alert('Login successfull!');
+      window.location.href="./adminpanel.html"
     } else {
       alert('Invalid username or password. Please try again.');
     }
