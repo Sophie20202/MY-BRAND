598f67660834e36fdc2e555302904a6cc092f985
Sophie20202 <sofidele12@gmail.com>
2024-02-07 22:06:00 -1200

    adding some js challenges

--- javascripts/login.js
+++ javascripts/login.js
@@ -1,13 +1,14 @@
 
-
 function validateForm(event) {
   event.preventDefault();
+ 
   var username = document.getElementById("email").value;
   var password = document.getElementById("password").value;
   console.log(password, username);
   if (username === 'sofidele12@gmail.com' && password === '12345') {
-    window.location.href="adminpanel.html";
     alert("Login successful!");
+    window.location.href="adminpanel.html";
+    
   } else {
     alert("Invalid username or password. Please try again.");
   }
