afc32d86d9e71dd4e870058f5f7e2e39178a23b9
Sophie20202 <sofidele12@gmail.com>
2024-02-07 17:58:08 -1200

    some changessss

--- javascripts/login.js
+++ javascripts/login.js
@@ -1,14 +1,18 @@
 
-function validateForm() {
-    var username = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-    console.log(password,username)
-    if (username === 'sofidele12@gmail.com' && password === 'sophie1992@') {
-      // alert('Login successfull!');
-      window.location.href="./adminpanel.html"
-    } else {
-      alert('Invalid username or password. Please try again.');
-    }
-    }
+
+function validateForm(event) {
+  event.preventDefault();
+  var username = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  const myemail = "sofidele12@gmail.com";
+  const mypassword = "sophie1992@";
+  console.log(password, username);
+  if (username === myemail && password === mypassword) {
+    window.location.href="adminpanel.html";
+    alert("Login successful!");
+  } else {
+    alert("Invalid username or password. Please try again.");
+  }
+}
    
 
