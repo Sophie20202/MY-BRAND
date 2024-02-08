91a64e586ae5bf3c7448076bf738f7fcb4beb9b9
Sophie20202 <sofidele12@gmail.com>
2024-02-07 18:20:09 -1200

    gdhfghfh

--- javascripts/login.js
+++ javascripts/login.js
@@ -4,10 +4,8 @@ function validateForm(event) {
   event.preventDefault();
   var username = document.getElementById("email").value;
   var password = document.getElementById("password").value;
-  const myemail = "sofidele12@gmail.com";
-  const mypassword = "sophie1992@";
   console.log(password, username);
-  if (username === myemail && password === mypassword) {
+  if (username === 'sofidele12@gmail.com' && password === '12345') {
     window.location.href="adminpanel.html";
     alert("Login successful!");
   } else {
