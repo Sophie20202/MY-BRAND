53e52a3c5628ca575c6c68223a8f0f38df895882
Sophie20202 <sofidele12@gmail.com>
2024-02-09 11:57:49 -1200

    dhcfdjjdhcjkhjkasda

--- javascripts/create-blog.js
+++ javascripts/create-blog.js
@@ -1,32 +1,26 @@
+
 window.addEventListener("load", function () {
   const form = document.getElementById("articleForm");
   form.addEventListener("submit", function (e) {
     e.preventDefault();
-    // function blog(){
+
     var title = document.getElementById("title").value;
-    // var  articleName= document.getElementById('articleName').value;
     var articleContent = document.getElementById("articleContent").value;
     var fileInput = document.getElementById("picture");
     var description = document.getElementById("description").value;
 
-    if (title === "" && articleName === "") {
-      alert("Please enter both title and content of blog.");
+    if (title === "" && articleContent === "") {
+      alert("Please enter both title and content of the blog.");
       return false;
     }
-    //  if(articleContent!="")
-    //  alert("Successfully submitted")
-    // }
 
     var object = {
       id: new Date().getTime(),
       title: title,
-      // articleName:articleName,
       articleContent: articleContent,
       description: description,
     };
 
-    // window.localStorage.setItem("bject", JSON.stringify(object));
-    // alert('blog stored succesfully');
     const processImage = (id, input) => {
       if (input.files && input.files[0]) {
         const reader = new FileReader();
@@ -49,3 +43,54 @@ window.addEventListener("load", function () {
     window.location.href = "blog.html";
   });
 });
+// window.addEventListener("load", function () {
+//   const form = document.getElementById("articleForm");
+//   form.addEventListener("submit", function (e) {
+//     e.preventDefault();
+//     // function blog(){
+//     var title = document.getElementById("title").value;
+//     // var  articleName= document.getElementById('articleName').value;
+//     var articleContent = document.getElementById("articleContent").value;
+//     var fileInput = document.getElementById("picture");
+//     var description = document.getElementById("description").value;
+
+//     if (title === "" && articleName === "") {
+//       alert("Please enter both title and content of blog.");
+//       return false;
+//     }
+//     //  if(articleContent!="")
+//     //  alert("Successfully submitted")
+//     // }
+
+//     var object = {
+//       id: new Date().getTime(),
+//       title: title,
+//       // articleName:articleName,
+//       articleContent: articleContent,
+//       description: description,
+//     };
+
+//     // window.localStorage.setItem("bject", JSON.stringify(object));
+//     // alert('blog stored succesfully');
+//     const processImage = (id, input) => {
+//       if (input.files && input.files[0]) {
+//         const reader = new FileReader();
+//         reader.onload = function (e) {
+//           const articles = JSON.parse(localStorage.getItem("articles")) || [];
+//           const article = articles.find((article) => article.id === id);
+
+//           article.image = e.target.result;
+//           localStorage.setItem("articles", JSON.stringify(articles));
+//         };
+//         reader.readAsDataURL(input.files[0]);
+//       }
+//     };
+   
+//     const storeblog = JSON.parse(localStorage.getItem("articles")) || [];
+//     storeblog.push(object);
+
+//     window.localStorage.setItem("articles", JSON.stringify(storeblog));
+//     processImage(object.id, fileInput);
+//     window.location.href = "blog.html";
+//   });
+// });
