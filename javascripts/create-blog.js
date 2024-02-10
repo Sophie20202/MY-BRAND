ceaf91022722ee9898519b1a43ea298c9f90b9e4
Sophie20202 <sofidele12@gmail.com>
2024-02-09 13:41:18 -1200

    fdfjhgvjmhgjhjh

--- javascripts/create-blog.js
+++ javascripts/create-blog.js
@@ -6,26 +6,22 @@ window.addEventListener("load", function () {
     e.preventDefault();
     
     var title = document.getElementById("title").value;
-    
     var articleContent = document.getElementById("articleContent").value;
     var fileInput = document.getElementById("picture");
     var description = document.getElementById("description").value;
 
-    if (title === "" && articleName === "") {
+    if (title === "" || articleContent === "") {
       alert("Please enter both title and content of blog.");
       return false;
     }
-   
 
     var object = {
       id: new Date().getTime(),
       title: title,
-      
       articleContent: articleContent,
       description: description,
     };
 
-   
     const processImage = (id, input) => {
       if (input.files && input.files[0]) {
         const reader = new FileReader();
@@ -33,17 +29,22 @@ window.addEventListener("load", function () {
           const articles = JSON.parse(localStorage.getItem("articles")) || [];
           const article = articles.find((article) => article.id === id);
 
-          article.image = e.target.result;
-          localStorage.setItem("articles", JSON.stringify(articles));
+          if (article) {
+            article.image = e.target.result;
+            localStorage.setItem("articles", JSON.stringify(articles));
+          }
         };
         reader.readAsDataURL(input.files[0]);
       }
     };
-   
-    const storeblog = JSON.parse(localStorage.getItem("articles")) || [];
-    storeblog.push(object);
 
-    window.localStorage.setItem("articles", JSON.stringify(storeblog));
+    let storeblog = JSON.parse(localStorage.getItem("articles")) || [];
+    if (!Array.isArray(storeblog)) {
+      storeblog = [];
+    }
+
+    storeblog.push(object);
+    localStorage.setItem("articles", JSON.stringify(storeblog));
     processImage(object.id, fileInput);
     window.location.href = "blog.html";
   });
